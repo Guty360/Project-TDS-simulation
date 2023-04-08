1a66d0841b56708fb937a68c7f230860099d9c31
FyreFox360 <jmge5833@gmail.com>
2023-04-08 17:18:30 -0600

    Agregando sistema dinamico parcial

--- project/functions.py
+++ project/functions.py
@@ -25,19 +25,37 @@ def MinimumData(CL1, CL2, CL3, CL4):
                 return CL4
 
 
-def array2(data, CL1, CL2, CL3, CL4):
-    asignacion = 1000
-    paso = 0
+def array2(paso, MC, CL1, CL2, CL3, CL4, n, ES, maqServer, maqCola):
+    paso = paso
+    maqCola = maqCola
+    maqServer = maqServer
     n = 0
     ES = 0
     MC = 0
-    MinimumData = data
 
     collection = [
-        paso, MC, CL1, CL2, CL3, CL4, n, n, n, n]
-    returnData(collection)
+        paso, MC, CL1, CL2, CL3, CL4, n, ES, maqServer, maqCola]
     return collection
 
 
 def returnData(collection):
-    print()
+    for i in range(len(collection)):
+        aux = collection[i]
+        return aux
+
+
+# def ValidationOfData(dataCollection, DataMinimun):
+#     Out1 = 1001
+#     Out2 = 1002
+#     Out3 = 1003
+#     Out4 = 1004
+#     AuxSever = 1
+#     for i in range(0, 4):
+#         if DataMinimun == dataCollection[2]:
+#             dataCollection[1] = DataMinimun
+#             dataCollection[2] = Out1
+#             dataCollection[5] = dataCollection[1] + 5
+#             dataCollection[6] = 1
+#             dataCollection[7] = 1
+#             dataCollection[8] = AuxSever
+#             return dataCollection
