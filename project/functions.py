b6f8b36084181b31928ab3e47dc815e457e6f6ef
FyreFox360 <jmge5833@gmail.com>
2023-04-08 12:22:47 -0600

    Generando asignacion dinamica

--- project/functions.py
+++ project/functions.py
@@ -1,11 +1,43 @@
-def saludar(nombre):
-    print(f"Hola, {nombre}")
+from constants import constantes
 
 
-def decirHola():
-    dato = "Hola como estan guapos"
-    return dato
+def MinimumData(CL1, CL2, CL3, CL4):
+    MC = 0
+    CL1 = CL1
+    CL2 = CL2
+    CL3 = CL3
+    CL4 = CL4
+    if (CL1 < CL2):
+        if (CL1 < CL3):
+            if (CL1 < CL4):
+                return CL1
+    if (CL2 < CL1):
+        if (CL2 < CL3):
+            if (CL2 < CL4):
+                return CL2
+    if (CL3 < CL1):
+        if (CL3 < CL2):
+            if (CL3 < CL4):
+                return CL3
+    if (CL4 < CL1):
+        if (CL4 < CL2):
+            if (CL4 < CL3):
+                return CL4
 
-def array():
-    elemento = ["valor 1", "valor 2"]
-    return elemento
+
+def array2(data, CL1, CL2, CL3, CL4):
+    asignacion = 1000
+    paso = 0
+    n = 0
+    ES = 0
+    MC = 0
+    MinimumData = data
+
+    collection = [
+        paso, MC, CL1, CL2, CL3, CL4, n, n, n, n]
+    returnData(collection)
+    return collection
+
+
+def returnData(collection):
+    print()
