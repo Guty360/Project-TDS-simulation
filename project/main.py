0af62011f046d3fd166af6b2d0b461d945f61157
Rosalioalfre2 <rosalioalfre2@gmail.com>
2023-04-11 19:06:35 -0600

    Un gran avance pero dudando
    el 10 de la fila automatica me cambia por el excel de claudia

--- project/main.py
+++ project/main.py
@@ -35,34 +35,48 @@ valores = {
 filas = [tuple(valores.values())]
 
 
-for i in range(0, 10):
-    """ clocksAuxiliar = clocks
-    clocksAuxiliar.append(clockAuxiliar) """
+for i in range(0, 15):    
     
     clocksAuxiliar = [valores["clock1"], valores["clock2"],valores["clock3"],valores["clock4"],valores["clock5"]]
     valores["MC"] = min(clocksAuxiliar)
 
-
     for element in out:
-        if not element in clocks:
-            if clockAuxiliar == 1000:
-                clockAuxiliar = clocksAuxiliar[clocksAuxiliar.index(min(clocksAuxiliar))] + 11
-            clocks[clocks.index(min(clocks))] = element
-            valores["ES"] = 1
+        if not element in clocksAuxiliar:
+
+            #Si el clock auxiliar es el minimo
+            if clocksAuxiliar.index(min(clocksAuxiliar)) == (len(clocksAuxiliar) - 1):
+                if clocksAuxiliar[len(clocksAuxiliar) - 1] == valores["MC"]:
+                    clocksAuxiliar[len(clocksAuxiliar) - 1] = valores["MC"] + 11
+                valores["ES"] = 1
+                
+                if clocksAuxiliar[len(clocksAuxiliar) - 1] != 1000:
+                    clocksAuxiliar[clocksAuxiliar.index(out[0])] = valores["MC"] + 480
+                break
+            
+            #Se asigna si el reloj auxiliar index
+            clocksAuxiliar[clocksAuxiliar.index(min(clocksAuxiliar))] = element
+
+            #Si el elemento es el primer out
+            if element == out[0]:
+                clocksAuxiliar[len(clocksAuxiliar) - 1] = valores["MC"] + 11
             break
-        
-    if valores["MC"] == clockAuxiliar:
-        clocks[clocks.index(min(out))] = valores["MC"] + 480
-        for i in range(1,4):
-            if out[i] in clocks:
-                clocks[clocks.index(out[i])] = out[i-1]
+    
+    if not out[0] in clocksAuxiliar:
+        contador = 0
+        for i in range(1,len(out)):
+            if out[i] in clocksAuxiliar:
+                clocksAuxiliar[clocksAuxiliar.index(out[i])] = out[i-1]
+                contador += 1
+        if contador == 0:
+            clocksAuxiliar[len(clocksAuxiliar) - 1] = 1000
+    
     
     # * Actualizando los valores
-    valores["clock1"] = clocks[0]
-    valores["clock2"] = clocks[1]
-    valores["clock3"] = clocks[2]
-    valores["clock4"] = clocks[3]
-    valores["clock5"] = clockAuxiliar
+    valores["clock1"] = clocksAuxiliar[0]
+    valores["clock2"] = clocksAuxiliar[1]
+    valores["clock3"] = clocksAuxiliar[2]
+    valores["clock4"] = clocksAuxiliar[3]
+    valores["clock5"] = clocksAuxiliar[4]
     
     valores["Paso"]+=1
     filas.append(tuple(valores.values()))
