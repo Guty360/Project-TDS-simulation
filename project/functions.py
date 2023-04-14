bbeb16ec64d2b8002a3def233a445dd83df36f1c
Rosalioalfre2 <rosalioalfre2@gmail.com>
2023-04-14 11:06:35 -0600

    Version 3.1
    * Dando formato al codigo
    * Bug solucionado, pasos exactos

--- project/functions.py
+++ project/functions.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 from tabulate import tabulate
 import random
+import json
 
 def ingresarValoresEnteros(indicacion):
     entrada = input(indicacion)
     if not entrada.isdigit():
         print("\nIngrese un numero entero valido! ")
         entrada = ingresarValoresEnteros(indicacion)
-    elif int(entrada) > 1000 or int(entrada) < 0:
-        print("\nIngrese un numero entero valido! (maximo: 1000, minimo: 0) ")
+    elif int(entrada) > 10000 or int(entrada) < 0:
+        print("\nIngrese un numero entero valido! (maximo: 10000, minimo: 0) ")
         entrada = ingresarValoresEnteros(indicacion)
     return int(entrada)
 
@@ -66,9 +67,9 @@ def ingresarDatosPorSemilla():
 def ingresarDatosPorUsuario():
     clocks = []
 
-    cantidadDeClocks = ingresarValoresEnteros("Cuantas maquinas, (clocks), le gustaria ingresar? (minimo: 3, maximo 10)\n")
+    cantidadDeClocks = ingresarValoresEnteros("Cuantas maquinas, (clocks), le gustaria ingresar? (minimo: 3)\n")
 
-    if cantidadDeClocks < 3 or cantidadDeClocks > 10:
+    if cantidadDeClocks < 3:
         clocks = ingresarDatosPorUsuario()
     else:
         for i in range(0, cantidadDeClocks):
@@ -80,13 +81,95 @@ def ingresarDatosPorUsuario():
 def ingresarDatosPorAletoriedad():
     clocks = []
 
-    cantidadDeClocks = ingresarValoresEnteros("Cuantas maquinas, (clocks), le gustaria ingresar? (minimo: 3, maximo: 10)\n")
+    cantidadDeClocks = ingresarValoresEnteros("Cuantas maquinas, (clocks), le gustaria ingresar? (minimo: 3)\n")
 
-    if cantidadDeClocks < 3 or cantidadDeClocks > 10:
+    if cantidadDeClocks < 3:
         print("\nChistosito, que no sabe leer?")
         clocks = ingresarDatosPorAletoriedad()
     
     for i in range(0, cantidadDeClocks):
-        clocks.append(random.randint(1, 100))
+        clocks.append(random.randint(1, 1000))
 
     return clocks
+
+def matriz(valores, ajuste, clocks, out, clockServidor):
+    filas = [tuple(valores.values())]
+
+    # Usamos un auxiliar y determinamos los outs
+    clocksAuxiliar = []
+    for i in range(0, len(clocks)+1):
+        clocksAuxiliar.append(valores["clock{}".format(i+1)])
+        out.append(out[0]+i)
+
+    for i in range(0, ajuste["cantidadDeEjecucciones"]):    
+
+        valores["MC"] = min(clocksAuxiliar)
+
+        for element in out:
+            if not element in clocksAuxiliar:
+                #Si el clock auxiliar es el minimo
+                if clocksAuxiliar.index(min(clocksAuxiliar)) == (len(clocksAuxiliar) - 1):
+                    if clocksAuxiliar[len(clocksAuxiliar) - 1] == valores["MC"]:
+                        clocksAuxiliar[len(clocksAuxiliar) - 1] = valores["MC"] + ajuste["tiempoSeteoYAjuste"]
+                    
+                    if clocksAuxiliar[len(clocksAuxiliar) - 1] != clockServidor:
+                        clocksAuxiliar[clocksAuxiliar.index(out[0])] = valores["MC"] + ajuste["tiempoNormalDeUsoDeMaquina"]
+                    break
+                
+                #Se asigna si el reloj auxiliar index
+                clocksAuxiliar[clocksAuxiliar.index(min(clocksAuxiliar))] = element
+                valores["ES"] = "Ocupado"
+
+                #Si el elemento es el primer out
+                if element == out[0]:
+                    clocksAuxiliar[len(clocksAuxiliar) - 1] = valores["MC"] + ajuste["tiempoSeteoYAjuste"]
+                break
+        
+        if not out[0] in clocksAuxiliar:
+            contador = 0
+            for i in range(1,len(out)):
+                if out[i] in clocksAuxiliar:
+                    clocksAuxiliar[clocksAuxiliar.index(out[i])] = out[i-1]
+                    contador += 1
+            if contador == 0:
+                clocksAuxiliar[len(clocksAuxiliar) - 1] = clockServidor
+                valores["ES"] = "Desocupado"
+
+        # * Obtenemos el numero de maquinas en proceso
+        valores["numero"] = 0
+        for element in out:
+            if element in clocksAuxiliar:
+                valores["numero"] += 1
+
+        # * Obtenemos la maquina en servicio
+        if out[0] in clocksAuxiliar:
+            valores["maqServer"] = clocksAuxiliar.index(out[0]) + 1
+        else:
+            valores["maqServer"] = "-"
+        
+        # * Obtenemos la maquina en cola
+        if out[1] in clocksAuxiliar:
+            valores["maqCola"] = clocksAuxiliar.index(out[1]) + 1
+        else:
+            valores["maqCola"] = "-"
+        
+        
+        # * Actualizando los valores
+        for i in range(1, len(clocksAuxiliar)+1):
+            valores["clock{}".format(i)] = clocksAuxiliar[i-1]
+        
+        valores["Paso"]+=1
+        # Para los casos especiales cuando al momento de salida y entrada coincida y se muestran dos Master Clock "MC" con el mismo el valor, es que se usa este y solo imprime una vez el master clock
+        if valores["clock{}".format(len(clocksAuxiliar))] != valores["MC"]:
+            filas.append(tuple(valores.values()))
+            i += 1
+
+    return filas
+
+def textoMatriz(ajuste, valores, matriz):
+    txt = "Los ajustes usados son:\n"
+    txt += json.dumps(ajuste)
+    txt += "\n\n"
+    txt += tabulate(matriz, headers=list(valores.keys()), tablefmt="orgtbl")
+
+    return txt
--- project/main.py
+++ project/main.py
@@ -1,25 +1,23 @@
-from functions import guardarEnTxt, cargarDatos
-from tabulate import tabulate
-import json
+from functions import guardarEnTxt, cargarDatos, matriz, textoMatriz
 
-# Variables
+#! Entradas
+## Datos a pedir al usuario
+### Pedimos los clocks
+clocks = cargarDatos()
+
+## Datos del sistema
+### Ajuste generales del sistema
 ajuste = {
     "tiempoSeteoYAjuste" : 11,
     "tiempoNormalDeUsoDeMaquina" : 480,
     "cantidadDeEjecucciones" : 25
 }
 
-
-## Datos a pedir al usuario
-### Pedimos los clocks (1-4)
-clocks = cargarDatos()
-
-    
 #* Clock del servidor
-clockServidor = 10000
+# clockServidor = max(clocks) * 10000
+clockServidor = 1000000
 
-## Datos del sistema
-out = [10001, 10002, 10003, 10004, 10005]
+out = [clockServidor + 1]
 valores = {
     "Paso" : 0,
     "MC": 0,
@@ -28,6 +26,7 @@ valores = {
 for i, clock in enumerate(clocks):
     key = "clock{}".format(i+1)
     valores[key] = clock
+
 #Agregamos el clock servidor
 key = "clock{}".format(len(clocks)+1)
 valores[key] = clockServidor
@@ -37,89 +36,19 @@ valores["ES"]= "Desocupado"
 valores["maqServer"]= "-"
 valores["maqCola"]= "-"
 
-    
-#* Guardamos las filas de la matriz a imprimir la final
-filas = [tuple(valores.values())]
 
 
-for i in range(0, ajuste["cantidadDeEjecucciones"]):    
-    
-    clocksAuxiliar = []
-    for i in range(0, len(clocks)+1):
-        clocksAuxiliar.append(valores["clock{}".format(i+1)])
+#! Proceso
+## Invocamos las funcion de la tabla
+matriz = matriz(valores, ajuste, clocks, out, clockServidor)
 
-    valores["MC"] = min(clocksAuxiliar)
 
-    for element in out:
-        if not element in clocksAuxiliar:
-            #Si el clock auxiliar es el minimo
-            if clocksAuxiliar.index(min(clocksAuxiliar)) == (len(clocksAuxiliar) - 1):
-                if clocksAuxiliar[len(clocksAuxiliar) - 1] == valores["MC"]:
-                    clocksAuxiliar[len(clocksAuxiliar) - 1] = valores["MC"] + ajuste["tiempoSeteoYAjuste"]
-                
-                if clocksAuxiliar[len(clocksAuxiliar) - 1] != clockServidor:
-                    clocksAuxiliar[clocksAuxiliar.index(out[0])] = valores["MC"] + ajuste["tiempoNormalDeUsoDeMaquina"]
-                break
-            
-            #Se asigna si el reloj auxiliar index
-            clocksAuxiliar[clocksAuxiliar.index(min(clocksAuxiliar))] = element
-            valores["ES"] = "Ocupado"
 
-            #Si el elemento es el primer out
-            if element == out[0]:
-                clocksAuxiliar[len(clocksAuxiliar) - 1] = valores["MC"] + ajuste["tiempoSeteoYAjuste"]
-            break
-    
-    if not out[0] in clocksAuxiliar:
-        contador = 0
-        for i in range(1,len(out)):
-            if out[i] in clocksAuxiliar:
-                clocksAuxiliar[clocksAuxiliar.index(out[i])] = out[i-1]
-                contador += 1
-        if contador == 0:
-            clocksAuxiliar[len(clocksAuxiliar) - 1] = clockServidor
-            valores["ES"] = "Desocupado"
+#! Salida
+txt = textoMatriz(ajuste, valores, matriz)
 
-    valores["numero"] = 0
-    # * Obtenemos el numero
-    for element in out:
-        if element in clocksAuxiliar:
-            valores["numero"] += 1
+## Imprimimos en consola los valores obtenidos
+print(txt)
 
-    # * Obtenemos la maquina en servicio
-    if out[0] in clocksAuxiliar:
-        valores["maqServer"] = clocksAuxiliar.index(out[0]) + 1
-    else:
-        valores["maqServer"] = "-"
-    
-    # * Obtenemos la maquina en cola
-    if out[1] in clocksAuxiliar:
-        valores["maqCola"] = clocksAuxiliar.index(out[1]) + 1
-    else:
-        valores["maqCola"] = "-"
-    
-    
-    # * Actualizando los valores
-    for i in range(1, len(clocksAuxiliar)+1):
-        valores["clock{}".format(i)] = clocksAuxiliar[i-1]
-    
-    valores["Paso"]+=1
-    # Para los casos especiales cuando al momento de salida y entrada coincida y se muestran dos Master Clock "MC" con el mismo el valor, es que se usa este y solo imprime una vez el master clock
-    if valores["clock{}".format(len(clocksAuxiliar))] != valores["MC"]:
-        filas.append(tuple(valores.values()))
-    else:
-        valores["Paso"] -= 1
-
-    
-print("\n")
-print("Los ajustes usados son:")
-print(ajuste)
-print("\n")
-print(tabulate(filas, headers=list(valores.keys()), tablefmt="orgtbl"))
-
-txt += "Los ajustes usados son:\n"
-txt += json.dumps(ajuste)
-txt += "\n\n"
-txt += tabulate(filas, headers=list(valores.keys()), tablefmt="orgtbl")
-
+## Guardamos en un txt los valores obtenidos
 guardarEnTxt(txt)
